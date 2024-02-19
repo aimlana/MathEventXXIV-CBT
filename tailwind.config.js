acee603e4899261d77d0be4444927347779523bc
Muh. Salim Maulana <salim.yaklu@gmail.com>
2024-02-19 13:48:38 +0800

    chore: :art: Configure Tailwind CSS settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./src/**/*.{html,js}",
+    "./resources/views/**/*.blade.php",
+    "./resources/views/**/*.js",
+    "./resources/views/**/*.vue",
+    "./storage/framework/views/*.php",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Figtree", ...defaultTheme.fontFamily.sans],
+        Poppins: ["Poppins", "serif"]
+      },
+      colors: {
+
+      },
+    },
   },
   plugins: [],
 }
