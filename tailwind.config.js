5618cfea3c32e15afcb8066e255a396f043c1d10
nurilmuqit <nurilmuqit@gmail.com>
2024-02-20 22:27:14 +0800

    feat: :sparkles: Install laravel jetstream
    for user login

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,23 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+import forms from '@tailwindcss/forms';
+import typography from '@tailwindcss/typography';
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./src/**/*.{html,js}",
-    "./resources/views/**/*.blade.php",
-    "./resources/views/**/*.js",
-    "./resources/views/**/*.vue",
-    "./storage/framework/views/*.php",
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ["Figtree", ...defaultTheme.fontFamily.sans],
-        Poppins: ["Poppins", "serif"]
-      },
-      colors: {
+    content: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './vendor/laravel/jetstream/**/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+    ],
 
-      },
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+            },
+        },
     },
-  },
-  plugins: [],
-}
 
+    plugins: [forms, typography],
+};
