9d536209322641d25b97731155e2c779cf43208e
Muh. Salim Maulana <salim.yaklu@gmail.com>
2024-02-27 23:33:33 +0800

    feat: :sparkles: Add soal-tes admin page

---
+++ resources/views/admin/soal-tes.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.general')
+
+{{-- Tab Menu Name --}}
+@section('title', 'Soal Tes')
+
+{{-- Sidebar --}}
+@section('sidebar')
+    <x-sidebar-admin></x-sidebar-admin>
+@endsection
+
+{{-- Soal Tes Main Content --}}
+@section('content')
+    
+@endsection
