710b59978f60271c12465d1843b33c819d2e7c3c
Muh. Salim Maulana <salim.yaklu@gmail.com>
2024-02-28 10:54:04 +0800

    style: :lipstick: Edit sidebar style

--- resources/views/components/sidebar-admin.blade.php
+++ resources/views/components/sidebar-admin.blade.php
@@ -10,7 +10,7 @@
             <a href="{{ route('admin.dashboard') }}"
                 class="flex items-center justify-start w-full py-4
                 {{ request()->routeIs('admin.dashboard') || (!str_contains(request()->route()->uri, 'admin') && !str_contains(request()->route()->uri, 'edit')) ? 'bg-wheat text-milkChoco' : '' }}">
-                <div class="flex items-center justify-center ps-8">
+                <div class="flex items-center justify-center px-10">
                     <i class="fa-solid fa-gauge text-xl pe-6"></i>
                     <h3 class="text-lg font-medium">Dashboard</h3>
                 </div>
@@ -18,7 +18,7 @@
             <a href="{{ route('admin.peserta') }}"
                 class="flex items-center justify-start w-full py-4
                 {{ request()->routeIs('admin.peserta') || (!str_contains(request()->route()->uri, 'admin') && !str_contains(request()->route()->uri, 'edit')) ? 'bg-wheat text-milkChoco' : '' }}">
-                <div class="flex items-center justify-center ps-8">
+                <div class="flex items-center justify-center px-10">
                     <i class="fa-solid fa-user-group text-xl pe-6"></i>
                     <h3 href="#" class="text-lg font-medium">Peserta</h3>
                 </div>
@@ -26,7 +26,7 @@
             <a href="{{ route('admin.soal-tes') }}"
                 class="flex items-center justify-start w-full py-4
                 {{ request()->routeIs('admin.soal-tes') || (!str_contains(request()->route()->uri, 'admin') && !str_contains(request()->route()->uri, 'edit')) ? 'bg-wheat text-milkChoco' : '' }}">
-                <div class="flex items-center justify-center ps-8">
+                <div class="flex items-center justify-center px-10">
                     <i class="fa-solid fa-list-check text-xl pe-6"></i>
                     <h3 href="#" class="text-lg font-medium">Soal Tes</h3>
                 </div>
@@ -34,7 +34,7 @@
             <a href="{{ route('admin.informasi') }}"
                 class="flex items-center justify-start w-full py-4
                 {{ request()->routeIs('admin.informasi') || (!str_contains(request()->route()->uri, 'admin') && !str_contains(request()->route()->uri, 'edit')) ? 'bg-wheat text-milkChoco' : '' }}">
-                <div class="flex items-center justify-center ps-8">
+                <div class="flex items-center justify-center px-10">
                     <i class="fa-solid fa-circle-exclamation text-xl pe-6"></i>
                     <h3 href="#" class="text-lg font-medium">Informasi</h3>
                 </div>
--- resources/views/components/sidebar-menu.blade.php
+++ resources/views/components/sidebar-menu.blade.php
@@ -2,8 +2,8 @@
     @yield('menu-content')
 
     {{-- Logout menu --}}
-    <div class="flex items-center justify-start w-full py-4 active:bg-wheat mt-44">
-        <div class="flex items-center justify-center ps-8">
+    <div class="flex items-center justify-start w-full py-4 hover:text-wheat mt-44">
+        <div class="flex items-center justify-center px-10">
             <i class="fa-solid fa-arrow-right-from-bracket text-xl pe-6"></i>
     
             <form method="POST" action="{{ route('logout') }}" x-data>
