19a88086386f46598c0304d90fc3fca9c9eb7dac
Muh. Salim Maulana <salim.yaklu@gmail.com>
2024-02-28 10:07:19 +0800

    feat: :sparkles: Add informasi & peserta page

---
+++ resources/views/admin/informasi.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.general')
+
+{{-- Tab Menu Name --}}
+@section('title', 'Informasi')
+
+{{-- Sidebar --}}
+@section('sidebar')
+    <x-sidebar-admin></x-sidebar-admin>
+@endsection
+
+{{-- Main Content --}}
+@section('content')
+    
+@endsection
---
+++ resources/views/admin/peserta.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.general')
+
+{{-- Tab Menu Name --}}
+@section('title', 'Peserta')
+
+{{-- Sidebar --}}
+@section('sidebar')
+    <x-sidebar-admin></x-sidebar-admin>
+@endsection
+
+{{-- Main Content --}}
+@section('content')
+    
+@endsection
