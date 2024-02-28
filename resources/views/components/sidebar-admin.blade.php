58a5da03af9505b1b57b880db3327c61a09f20b8
Muh. Salim Maulana <salim.yaklu@gmail.com>
2024-02-29 00:11:18 +0800

    style: :lipstick: Add hover effect to sidebar menu items

--- resources/views/components/sidebar-admin.blade.php
+++ resources/views/components/sidebar-admin.blade.php
@@ -8,32 +8,33 @@
         <x-sidebar-menu>
         @section('menu-content')
             <a href="{{ route('admin.dashboard') }}"
-                class="flex items-center justify-start w-full py-4
-                {{ request()->routeIs('admin.dashboard') || (!str_contains(request()->route()->uri, 'admin') && !str_contains(request()->route()->uri, 'edit')) ? 'bg-wheat text-milkChoco' : '' }}">
+                class="flex items-center justify-start w-full py-4 
+                {{ request()->routeIs('admin.dashboard') || (!str_contains(request()->route()->uri, 'admin') && !str_contains(request()->route()->uri, 'edit')) ? 'bg-wheat text-milkChoco' : ' hover:text-wheat' }}">
                 <div class="flex items-center justify-center px-10">
                     <i class="fa-solid fa-gauge text-xl pe-6"></i>
                     <h3 class="text-lg font-medium">Dashboard</h3>
                 </div>
             </a>
             <a href="{{ route('admin.peserta') }}"
-                class="flex items-center justify-start w-full py-4
-                {{ request()->routeIs('admin.peserta') || (!str_contains(request()->route()->uri, 'admin') && !str_contains(request()->route()->uri, 'edit')) ? 'bg-wheat text-milkChoco' : '' }}">
+                class="flex items-center justify-start w-full py-4 
+                {{ request()->routeIs('admin.peserta') ? 'bg-wheat text-milkChoco' : ' hover:text-wheat' }}">
                 <div class="flex items-center justify-center px-10">
                     <i class="fa-solid fa-user-group text-xl pe-6"></i>
                     <h3 href="#" class="text-lg font-medium">Peserta</h3>
                 </div>
             </a>
+
             <a href="{{ route('admin.soal-tes') }}"
-                class="flex items-center justify-start w-full py-4
-                {{ request()->routeIs('admin.soal-tes') || (!str_contains(request()->route()->uri, 'admin') && !str_contains(request()->route()->uri, 'edit')) ? 'bg-wheat text-milkChoco' : '' }}">
+                class="flex items-center justify-start w-full py-4 
+                {{ request()->routeIs('admin.soal-tes') || (!str_contains(request()->route()->uri, 'admin') && !str_contains(request()->route()->uri, 'edit')) ? 'bg-wheat text-milkChoco' : ' hover:text-wheat' }}">
                 <div class="flex items-center justify-center px-10">
                     <i class="fa-solid fa-list-check text-xl pe-6"></i>
                     <h3 href="#" class="text-lg font-medium">Soal Tes</h3>
                 </div>
             </a>
             <a href="{{ route('admin.informasi') }}"
-                class="flex items-center justify-start w-full py-4
-                {{ request()->routeIs('admin.informasi') || (!str_contains(request()->route()->uri, 'admin') && !str_contains(request()->route()->uri, 'edit')) ? 'bg-wheat text-milkChoco' : '' }}">
+                class="flex items-center justify-start w-full py-4 
+                {{ request()->routeIs('admin.informasi') || (!str_contains(request()->route()->uri, 'admin') && !str_contains(request()->route()->uri, 'edit')) ? 'bg-wheat text-milkChoco' : ' hover:text-wheat' }}">
                 <div class="flex items-center justify-center px-10">
                     <i class="fa-solid fa-circle-exclamation text-xl pe-6"></i>
                     <h3 href="#" class="text-lg font-medium">Informasi</h3>
--- resources/views/components/sidebar-user.blade.php
+++ resources/views/components/sidebar-user.blade.php
@@ -1,9 +1,9 @@
 <x-sidebar>
     @section('profile')
-        <div class="rounded-full w-28 h-28">
+        <a href="#" class="rounded-full w-28 h-28 hover:drop-shadow-xl">
             <img src="{{ asset('images/profile-pict.jpg') }}" alt="profile-picture"
                 class="object-cover rounded-full w-full h-full">
-        </div>
+        </a>
         <h1 class="font-medium text-xl mt-6">Go Youn Jung</h1>
     @endsection
 
@@ -12,7 +12,7 @@
         @section('menu-content')
             <a href="{{ route('user.profile') }}"
                 class="flex items-center justify-start w-full py-4
-                {{ request()->routeIs('user.profile') || (!str_contains(request()->route()->uri, 'user') && !str_contains(request()->route()->uri, 'edit')) ? 'bg-wheat text-milkChoco' : '' }}">
+                {{ request()->routeIs('user.profile') || (!str_contains(request()->route()->uri, 'user') && !str_contains(request()->route()->uri, 'edit')) ? 'bg-wheat text-milkChoco' : ' hover:text-wheat' }}">
                 <div class="flex items-center justify-center ps-8">
                     <i class="fa-solid fa-circle-user text-xl pe-6"></i>
                     <h3 href="#" class="text-lg font-medium">Profile</h3>
@@ -20,7 +20,7 @@
             </a>
             <a href="{{ route('user.ujian') }}"
                 class="flex items-center justify-start w-full py-4
-                {{ request()->routeIs('user.ujian') || (!str_contains(request()->route()->uri, 'user') && !str_contains(request()->route()->uri, 'edit')) ? 'bg-wheat text-milkChoco' : '' }}">
+                {{ request()->routeIs('user.ujian') || (!str_contains(request()->route()->uri, 'user') && !str_contains(request()->route()->uri, 'edit')) ? 'bg-wheat text-milkChoco' : ' hover:text-wheat' }}">
                 <div class="flex items-center justify-center ps-8">
                     <i class="fa-solid fa-file-lines text-xl pe-6"></i>
                     <h3 href="#" class="text-lg font-medium">Ujian</h3>
@@ -28,7 +28,7 @@
             </a>
             <a href="{{ route('user.informasi') }}"
                 class="flex items-center justify-start w-full py-4
-                {{ request()->routeIs('user.informasi') || (!str_contains(request()->route()->uri, 'user') && !str_contains(request()->route()->uri, 'edit')) ? 'bg-wheat text-milkChoco' : '' }}">
+                {{ request()->routeIs('user.informasi') || (!str_contains(request()->route()->uri, 'user') && !str_contains(request()->route()->uri, 'edit')) ? 'bg-wheat text-milkChoco' : ' hover:text-wheat' }}">
                 <div class="flex items-center justify-center ps-8">
                     <i class="fa-solid fa-circle-exclamation text-xl pe-6"></i>
                     <h3 href="#" class="text-lg font-medium">Informasi</h3>
