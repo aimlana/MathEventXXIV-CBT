5e8df2e0e3ba4a876d7b9e91c6987ad1150f9319
Muh. Salim Maulana <salim.yaklu@gmail.com>
2024-03-02 19:30:44 +0800

    fix: :art: Fix reusable table component for reusability

--- resources/views/components/table.blade.php
+++ resources/views/components/table.blade.php
@@ -4,7 +4,7 @@
             <div class="rounded-t mb-0 px-4 py-3 bg-milkChoco border-0">
                 <div class="flex flex-wrap items-center py-2">
                     <div class="relative w-full px-4 max-w-full flex-grow flex-1">
-                        <h3 class="font-semibold text-base text-white">Bank Soal</h3>
+                        <h3 class="font-semibold text-base text-white">@yield('title-table')</h3>
                     </div>
                     <div class="relative w-full px-4 max-w-full flex-grow flex-1 text-right">
                         @yield('button')
@@ -15,96 +15,11 @@
             <div class="block w-full overflow-x-auto">
                 <table class="items-center w-full border-collapse bg-wheat">
                     <thead>
-                        <tr>
-                            <th
-                                class="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-tan py-3 text-xs border-t-0 border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
-                                #
-                            </th>
-                            <th
-                                class="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-tan py-3 text-xs border-t-0 border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
-                                Nama Tes
-                            </th>
-                            <th
-                                class="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-tan py-3 text-xs border-t-0 border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
-                                Status
-                            </th>
-                            <th
-                                class="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-tan py-3 text-xs border-t-0 border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
-                                Soal
-                            </th>
-                            <th
-                                class="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-tan py-3 text-xs border-t-0 border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
-                                Tingkat
-                            </th>
-                            <th
-                                class="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-tan py-3 text-xs border-t-0 border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
-                                Tanggal Ujian
-                            </th>
-                            <th
-                                class="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-tan py-3 text-xs border-t-0 border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
-                                Durasi
-                            </th>
-                            <th
-                                class="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-tan py-3 text-xs border-t-0 border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
-                                Aksi
-                            </th>
-                        </tr>
+                        @yield('table-head')
                     </thead>
 
                     <tbody>
-                        <tr>
-                            <th
-                                class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left text-blueGray-700 ">
-                                1
-                            </th>
-                            <td
-                                class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 ">
-                                Penyisihan Mathematic <br>Event 24
-                            </td>
-                            <td
-                                class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                                <div class="py-1.5 bg-green-600 text-white flex justify-center items-center rounded">
-                                    Tersedia</div>
-                            </td>
-                            <td
-                                class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                                <div class="flex flex-col justify-center">
-                                    <div
-                                        class="py-1.5 bg-milkChoco text-white flex justify-center items-center rounded">
-                                        <i class="fa-regular fa-note-sticky pe-2"></i>25 PG</div>
-                                    <div
-                                        class="py-1.5 bg-milkChoco text-white flex justify-center items-center rounded mt-1">
-                                        <i class="fa-regular fa-note-sticky pe-2"></i>1 ESSY</div>
-                                </div>
-                            </td>
-                            <td
-                                class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                                <div class="py-1.5 bg-gray-400 text-white flex justify-center items-center rounded">
-                                    SMA</div>
-                            </td>
-                            <td
-                                class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                                <div
-                                        class="py-1.5 bg-orange-500 text-white flex justify-center items-center rounded mt-1">
-                                        <i class="fa-solid fa-calendar-days pe-2"></i>Rabu, 28 Februari 2024</div>
-                            </td>
-                            <td
-                                class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                                <div class="py-1.5 bg-amber-400 text-white flex justify-center items-center rounded">
-                                    120 menit</div>
-                            </td>
-                            <td
-                                class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                                <div class="flex flex-col justify-center">
-                                    <button type="button"
-                                        class="py-2 bg-sky-600 text-white flex justify-center items-center rounded">
-                                        <i class="fa-solid fa-pen"></i></button>
-                                    <button type="button"
-                                        class="py-2 bg-red-500 text-white flex justify-center items-center rounded mt-1">
-                                        <i class="fa-solid fa-trash"></i></button>
-                                </div> 
-                            </td>
-                        </tr>
+                        @yield('table-content')
                     </tbody>
                 </table>
             </div>
