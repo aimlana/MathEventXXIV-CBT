5c37415fb773e008a8dbe8b7808e44b241560a54
Muh. Salim Maulana <salim.yaklu@gmail.com>
2024-03-08 00:47:30 +0800

    feat: :sparkles: Add soal-essay page

---
+++ resources/views/admin/soal-essay.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.general')
+
+{{-- Tab Menu Name --}}
+@section('title', 'Informasi')
+
+{{-- Sidebar --}}
+@section('sidebar')
+    <x-sidebar-admin></x-sidebar-admin>
+@endsection
+
+{{-- Main Content --}}
+@section('content')
+    <div class="flex flex-col justify-start">
+        <div class="flex justify-between items-center">
+            <h2 class="font-bold text-xl text-milkChoco mt-6">Pilihan Ganda</h2>
+            <button type="submit" class="bg-wheat text-sm text-milkChoco font-medium py-3 px-8 rounded-md">Upload</button>
+        </div>
+
+        {{-- File Input (.pdf) --}}
+        <div class="w-3/4 h-72 border border-milkChoco border-dashed rounded-md self-center flex flex-col justify-center items-center mt-12">
+            <i class="fa-solid fa-file-pdf text-milkChoco text-xl"></i>
+            <p class="text-sm text-tan font-light mt-4">Silakan masukkan soal dalam bentuk gambar yah</p>
+            <p class="italic text-sm text-gray-400 mt-2">jpg (maks 500KB)</p>
+            <input type="file" class="hidden">
+        </div>
+
+        <button class="bg-milkChoco text-sm text-white font-medium py-3 rounded-md w-1/5 self-center mt-40">Submit</button>
+    </div>
+@endsection
