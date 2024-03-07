fc49a2ceaba20531a7e177b2609f1a831023e8ec
Muh. Salim Maulana <salim.yaklu@gmail.com>
2024-03-07 15:01:20 +0800

    feat: :art: Complete UI for tambah-informasi

--- resources/views/admin/tambah-informasi.blade.php
+++ resources/views/admin/tambah-informasi.blade.php
@@ -10,22 +10,40 @@
 
 {{-- Main Content --}}
 @section('content')
-    <form action="" method="POST" class="mt-8 ms-3">
-        <div class="flex items-start justify-between me-40">
+    <form action="" method="POST" class="mt-8 ms-3 flex flex-col me-32">
+        {{-- Information Title --}}
+        <div class="flex items-start justify-between">
             <label for="nama-tes" class="text-tan italic font-medium">Judul Berita</label>
-            <input type="text"
-                class="w-3/4 drop-shadow-md border-milkChoco focus:border-deer rounded-md">
+            <input type="text" class="w-3/4 drop-shadow-md border-milkChoco focus:border-deer rounded-md">
         </div>
-        <div class="flex items-start justify-between me-40 mt-8">
+
+        {{-- Information Release --}}
+        <div class="flex items-start justify-between mt-8">
             <label for="nama-tes" class="text-tan italic font-medium">Tanggal Berita</label>
-            <input type="text" placeholder="Pilih Tanggal" 
-                onfocus="(this.type='date')"
-                onblur="(this.type='text')"
+            <input type="text" placeholder="Pilih Tanggal" onfocus="(this.type='date')" onblur="(this.type='text')"
                 class="w-3/4 drop-shadow-md border-milkChoco focus:border-deer rounded-md placeholder:text-milkChoco text-center focus:">
         </div>
-        <div class="flex items-start justify-between me-40 mt-8">
+
+        {{-- Description --}}
+        <div class="flex items-start justify-between mt-8">
             <label for="nama-tes" class="text-tan italic font-medium">Deskripsi</label>
-            <textarea name="" id="" cols="30" rows="6" class="w-3/4 drop-shadow-md border-milkChoco focus:border-deer rounded-md"></textarea>
+            <textarea name="" id="" cols="30" rows="6"
+                class="w-3/4 drop-shadow-md border-milkChoco focus:border-deer rounded-md"></textarea>
         </div>
+
+        {{-- File Input (.pdf) --}}
+        <div class="flex items-start justify-between mt-8">
+            <label for="nama-tes" class="text-tan italic font-medium">Pengumuman</label>
+            <div class="w-3/4 h-44 border border-milkChoco border-dashed rounded-md flex flex-col justify-center items-center">
+                <i class="fa-solid fa-file-pdf text-red-600 text-xl"></i>
+                <p class="italic text-sm text-gray-300 font-light mt-4">Upload pengumuman kelulusan dalam bentuk pdf yah</p>
+                <input type="text" class="w-3/4 drop-shadow-md border-milkChoco focus:border-deer rounded-md hidden">
+            </div>
+        </div>
+
+        {{-- Submit Button --}}
+        <x-button class="mt-8 w-1/4 self-end">
+            {{ __('Submit') }}
+        </x-button>
     </form>
 @endsection
