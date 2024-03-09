b74c4907076f8c5b0f7a3784a027dadcb64e9d2a
nurilmuqit <nurilmuqit@gmail.com>
2024-03-09 13:06:08 +0800

    build: :art: Create a new table

--- app/Models/JawabanMahasiswa.php
+++ app/Models/JawabanMahasiswa.php
@@ -6,13 +6,13 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
-class Score extends Model
+class JawabanMahasiswa extends Model
 {
     use HasFactory;
     protected $fillable = [
         'user_id',
         'sub_soal_id',
-        'nilai',
+        'jawaban',
     ];
     public function User(): BelongsTo
     {
